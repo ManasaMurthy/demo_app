1d613aee58c86921b297dd13cc82849f9daf3f84
ManasaMurthy <manasa.n.murthy@gmail.com>
2017-12-26 14:31:21 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
     String home() {
-        return "Sample MicroService -Version 3";
+        return "Sample Application - Version 1";
     }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
